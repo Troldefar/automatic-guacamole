3b2fc041b24a75abd3837035a5e7b834b4b80cc2
troldefar <r_emil@live.dk>
2022-03-21 20:45:15 +0100

    spl

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -15,26 +15,14 @@ class Bootstrap {
             $define->define();
 
             require_once ROOT_PATH . 'src' . DS . 'interfaces' . DS . 'ValidationRuleInterface.php';
-            require_once ROOT_PATH . 'src' . DS . 'Entity.php';
-            require_once ROOT_PATH . 'src' . DS . 'Json.php';
-            require_once ROOT_PATH . 'src' . DS . 'Controller.php';
-            require_once ROOT_PATH . 'src' . DS . 'Template.php';
-            require_once ROOT_PATH . 'src' . DS . 'Session.php';
-            require_once ROOT_PATH . 'src' . DS . 'Database.php';
-            require_once ROOT_PATH . 'src' . DS . 'Router.php';
-            require_once ROOT_PATH . 'src' . DS . 'Validation.php';
-            require_once ROOT_PATH . 'src' . DS . 'Auth.php';
-
-            require_once ROOT_PATH . 'src/ValidationRules' . DS . 'ValidateMinimum.php';
-            require_once ROOT_PATH . 'src/ValidationRules' . DS . 'ValidateMaximum.php';
-            require_once ROOT_PATH . 'src/ValidationRules' . DS . 'ValidateEmail.php';
-            require_once ROOT_PATH . 'src/ValidationRules' . DS . 'ValidateCharacters.php';
-            require_once ROOT_PATH . 'src/ValidationRules' . DS . 'ValidateTrim.php';
 
             spl_autoload_register(function($class) {
-                echo $class;
                 $file = ROOT_PATH . 'src' . DS . $class . '.php';
-                echo ROOT_PATH . 'src' . DS . $class . '.php';
+                if (file_exists($file)) require_once $file;
+            });
+
+            spl_autoload_register(function($class) {
+                ROOT_PATH . 'src/ValidationRules' . DS . $class . '.php';
                 if (file_exists($file)) require_once $file;
             });
 
